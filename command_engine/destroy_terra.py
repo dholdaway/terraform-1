a2a39b0075f8462585b39d776f62ccbb110890d9
maxwellflitton <maxwellflitton@gmail.com>
2022-02-25 12:32:21 +0000

    adding destroy

---
+++ command_engine/destroy_terra.py
@@ -0,0 +1,35 @@
+"""
+This script defines the entry point for terra-destroy.
+"""
+import os
+from pathlib import Path
+from subprocess import Popen
+
+from command_engine.config_loader import ConfigEngine
+
+
+def main() -> None:
+    """
+    Loads the data from the terra_config.yml config file in the current directory and run a terraform apply command.
+
+    :return: None
+    """
+    config_path: str = str(os.getcwd()) + "/terra_config.yml"
+    file_path: str = str(Path(__file__).parent)
+
+    config = ConfigEngine(config_path=config_path)
+
+    command_buffer = [f'cd {file_path}/{config["location"]} ', '&& ', 'terraform destroy ']
+    variables = config["variables"]
+
+    for key in variables:
+        command_buffer.append(f'-var="{key}={variables[key]}" ')
+
+    command = "".join(command_buffer)
+    print("applying the following command: ")
+    print(command)
+
+    continue_running = input("please type 'yes': ")
+    if continue_running == "yes":
+        run_terraform = Popen(command, shell=True)
+        run_terraform.wait()
--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
    entry_points={
        "console_scripts": [
            "terra-apply=command_engine.run_terra:main",
+           "terra-destroy=command_engine.destroy_terra:main",
         ]
    },
 )
